759d375b75208e8880136169e55b2760078cbb9d
maksimyakush <maksimo-1988@yandex.ru>
2022-09-10 13:59:26 +0300

    chore: initial build

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import Example from './example.mdx';
+
+export const App = () => {
+  return (
+      <div
+        style={{
+          margin: "0 auto",
+          maxWidth: "1200px",
+          minHeight: "50vh",
+          backgroundColor: "pink",
+        }}
+      >
+        <Example />
+
+      </div>
+  );
+};
